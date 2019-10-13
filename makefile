febff2eb72fe9509fa7e78e34c52d78ddfc4203a
Ebrahim Alhaddad <ebrahimalhaddad@ebrahims-mbp-2.home>
2019-10-12 18:36:32 -0700

    check p2

--- makefile
+++ makefile
@@ -1,18 +1,21 @@
 CC=g++
 
-all: p1_a p1_b p1_serial.o p1_serial.ex
+all: p1_serial p1_a p1_b p2_serial p2
 p1_a: p1_a.cpp
 	g++ -lrt -fopenmp -o p1_a p1_a.cpp
 
 p1_b: p1_b.cpp
 	g++ -lrt -fopenmp -o p1_b p1_b.cpp
 
-p1_serial.o: p1_serial.c
-	gcc -c p1_serial.c
+p1_serial: p1_serial.c
+	g++ -o p1_serial p1_serial.c
 
-p1_serial.ex: p1_serial.o
-	gcc -o p1_serial p1_serial.o
-	
+p2_serial: p2_serial.c
+	g++ -o p2_serial p2_serial.c
+
+p2: p2.cpp
+	g++ -lrt -fopenmp -o p2 p2.cpp
+
 .PHONY: clean
 
 clean:
---
+++ p2_serial.cpp
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+ 
+int Partition(int a[], int start, int end)
+{
+    int pivot, index, i;
+    index = start;
+    pivot = end;
+ 
+    for(i=start; i < end; i++)
+    {
+        if(a[i] < a[pivot])
+        {
+            swap(&a[i], &a[index]);
+            index++;
+        }
+    }
+    
+    swap(&a[pivot], &a[index]);
+    return index;
+}
+ 
+int RandomPivotPartition(int a[], int start, int end)
+{
+    int pvt, n, temp;
+    n = rand();
+    pvt = start + n%(end-start+1);
+    swap(&a[end], &a[pvt]);
+    return Partition(a, start, end);
+}
+ 
+int quickSort(int a[], int start, int end)
+{
+    int pindex;
+    if(start < end)
+    {
+        pindex = RandomPivotPartition(a, start, end);
+        quickSort(a, start, pindex-1);
+        quickSort(a, pindex+1, end);
+    }
+    return 0;
+}
+
+int main(void){
+	int i, j, tmp;
+    int size = 16*1024*1024;
+	struct timespec start, stop;
+	double exe_time;
+	srand(time(NULL)); 
+	int * m = new int[size];
+	for(i=0; i<size; i++){
+		m[i]=size-i;
+	}
+	
+	if( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+	////////**********Your code goes here***************//
+    quickSort(m,0,size-1);
+	///////******************************////
+	
+	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}		
+	exe_time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+	
+	for(i=0;i<16;i++) printf("%d ", m[i]);		
+	printf("\nExecution time = %f sec\n",  exe_time);
+}	

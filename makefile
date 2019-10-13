24ad3507ffdf0cbafad25bbc1f4efd40e8f4cb72
Ebrahim Alhaddad <ebrahimalhaddad@ebrahims-mbp-2.home>
2019-10-12 17:20:15 -0700

    6

--- makefile
+++ makefile
@@ -1,9 +1,18 @@
 CC=g++
 
-all: serial_o
-serial_o: p1_serial.cpp
-	g++ -lrt -fopenmp -o p1_a p1_serial.cpp
+all: p1_a p1_b p1_serial.o p1_serial.ex
+p1_a: p1_a.cpp
+	g++ -lrt -fopenmp -o p1_a p1_a.cpp
 
+p1_b: p1_b.cpp
+	g++ -lrt -fopenmp -o p1_b p1_b.cpp
+
+p1_serial.o: p1_serial.c
+	gcc -c p1_serial.c
+
+p1_serial.ex: p1_serial.o
+	gcc -o p1_serial.o
+	
 .PHONY: clean
 
 clean:
---
+++ p1_b.cpp
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+typedef struct{
+	double x;  
+	double y;
+}Point; 
+
+int main(void){
+    int num_of_points=40000000;
+    omp_set_num_threads(4);
+	int i,tid;
+    int chunk = num_of_points/4;
+	int num_of_points_in_circle;
+	double pi;
+	struct timespec start, stop; 
+	double time;
+	Point * data_point = new Point[num_of_points];
+	for(i=0; i<num_of_points; i++){
+		data_point[i].x=(double)rand()/(double)RAND_MAX;
+		data_point[i].y=(double)rand()/(double)RAND_MAX;
+	}
+	num_of_points_in_circle=0;
+	
+	if( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+	
+	
+	////////**********Use OpenMP to parallize this loop***************//
+#pragma omp parallel shared(data_point,num_of_points) private(i,tid) reduction(+: num_of_points_in_circle)
+{
+    tid = omp_get_thread_num();
+    #pragma omp for schedule(static,chunk) nowait
+    for(i=0; i<num_of_points; i++){
+        if((data_point[i].x-0.5)*(data_point[i].x-0.5)+(data_point[i].y-0.5)*(data_point[i].y-0.5)<=0.25){
+            num_of_points_in_circle++;
+        }
+    }
+}
+		
+	///////******************************////
+	
+	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}		
+	time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+		
+	pi =4*(double)num_of_points_in_circle/(double)num_of_points;
+	printf("Estimated pi is %f, execution time = %f sec\n",  pi, time);		
+}	
